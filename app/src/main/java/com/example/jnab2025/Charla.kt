c816d9c8e621b8d476fe62bf9738772dbae7a612
ToroSantiago <santiagotoro89@gmail.com>
2025-05-19 15:45:32 -0300

    abm simpsios, abm propuestas

---
+++ app/src/main/java/com/example/jnab2025/Charla.kt
@@ -0,0 +1,16 @@
+package com.example.jnab2025
+
+import java.time.LocalTime
+
+data class Charla(
+    val id: Int,
+    val titulo: String,
+    val descripcion: String,
+    val expositor: String,
+    var fecha: String, // Formato "yyyy-MM-dd"
+    var horaInicio: String, // Formato "HH:mm"
+    var horaFin: String, // Formato "HH:mm"
+    var sala: String,
+    var esFavorito: Boolean = false,
+    val esDestacado: Boolean = false
+)
